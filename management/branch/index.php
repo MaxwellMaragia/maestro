cfc9e8691f1987133fbc68996df8b8b5dc54c2b0
unknown <maxmaragia@gmail.com>
2019-01-21 14:47:25 +0300

    resit payment system

---
+++ management/branch/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Bubbly - Boootstrap 4 Admin template by Bootstrapious.com</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all,follow">
+    <?php include 'plugins/resources.php'?>
+
+</head>
+<body>
+<!-- navbar-->
+<?php include 'plugins/navigation.php'?>
+<!--end navbar-->
+    <div class="page-holder w-100 d-flex flex-wrap">
+        <div class="container-fluid px-xl-5">
+<!--         code here-->
+
+
+
+        </div>
+        <!--   footer-->
+        <?php include 'plugins/footer.php'?>
+        <!--        end footer-->
+    </div>
+</div>
+<!-- JavaScript files-->
+<?php include 'plugins/scripts.php'?>
+</body>
+</html>
---
+++ management/branch/plugins/scripts.php
@@ -0,0 +1,8 @@
+<script src="../assets/vendor/jquery/jquery.min.js"></script>
+<script src="../assets/vendor/popper.js/umd/popper.min.js"> </script>
+<script src="../assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="../assets/vendor/jquery.cookie/jquery.cookie.js"> </script>
+<script src="../assets/vendor/chart.js/Chart.min.js"></script>
+<script src="../assets/https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
+<script src="../assets/js/charts-home.js"></script>
+<script src="../assets/js/front.js"></script>
